ffce825b2ebaf488ea37657f1defa8bca1e31609
storm_999_ <storm_999_@hotmail.com>
2018-04-13 23:28:06 +0200

    master

---
+++ Remote Flight Controller/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Remote_Flight_Controller
+{
+    partial class RemoteFlightController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.ip_txtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Port_txtBox = new System.Windows.Forms.TextBox();
+            this.port_label = new System.Windows.Forms.Label();
+            this.elevatorUp_btn = new System.Windows.Forms.Button();
+            this.elevatorDown_btn = new System.Windows.Forms.Button();
+            this.throttleDown_btn = new System.Windows.Forms.Button();
+            this.throttleUp_btn = new System.Windows.Forms.Button();
+            this.Throttle_label = new System.Windows.Forms.Label();
+            this.elevator_label = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.airSpeed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.verticalSpeed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pitchAngle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Warning = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.Location = new System.Drawing.Point(187, 26);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(110, 26);
+            this.connect_btn.TabIndex = 0;
+            this.connect_btn.Text = "Connect";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connectBTN_Click);
+            // 
+            // ip_txtBox
+            // 
+            this.ip_txtBox.Location = new System.Drawing.Point(12, 28);
+            this.ip_txtBox.Name = "ip_txtBox";
+            this.ip_txtBox.Size = new System.Drawing.Size(92, 20);
+            this.ip_txtBox.TabIndex = 1;
+            this.ip_txtBox.Text = "77.109.163.250";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // Port_txtBox
+            // 
+            this.Port_txtBox.Location = new System.Drawing.Point(115, 30);
+            this.Port_txtBox.Name = "Port_txtBox";
+            this.Port_txtBox.Size = new System.Drawing.Size(57, 20);
+            this.Port_txtBox.TabIndex = 3;
+            this.Port_txtBox.Text = "9999";
+            // 
+            // port_label
+            // 
+            this.port_label.AutoSize = true;
+            this.port_label.Location = new System.Drawing.Point(115, 9);
+            this.port_label.Name = "port_label";
+            this.port_label.Size = new System.Drawing.Size(26, 13);
+            this.port_label.TabIndex = 4;
+            this.port_label.Text = "Port";
+            // 
+            // elevatorUp_btn
+            // 
+            this.elevatorUp_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.elevatorUp_btn.Location = new System.Drawing.Point(210, 95);
+            this.elevatorUp_btn.Name = "elevatorUp_btn";
+            this.elevatorUp_btn.Size = new System.Drawing.Size(29, 47);
+            this.elevatorUp_btn.TabIndex = 5;
+            this.elevatorUp_btn.Text = "↑";
+            this.elevatorUp_btn.UseVisualStyleBackColor = true;
+            this.elevatorUp_btn.Click += new System.EventHandler(this.elevatorUp_btn_Click);
+            // 
+            // elevatorDown_btn
+            // 
+            this.elevatorDown_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.elevatorDown_btn.Location = new System.Drawing.Point(210, 148);
+            this.elevatorDown_btn.Name = "elevatorDown_btn";
+            this.elevatorDown_btn.Size = new System.Drawing.Size(29, 47);
+            this.elevatorDown_btn.TabIndex = 6;
+            this.elevatorDown_btn.Text = "↓";
+            this.elevatorDown_btn.UseVisualStyleBackColor = true;
+            this.elevatorDown_btn.Click += new System.EventHandler(this.elevatorDown_btn_Click);
+            // 
+            // throttleDown_btn
+            // 
+            this.throttleDown_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.throttleDown_btn.Location = new System.Drawing.Point(71, 148);
+            this.throttleDown_btn.Name = "throttleDown_btn";
+            this.throttleDown_btn.Size = new System.Drawing.Size(29, 47);
+            this.throttleDown_btn.TabIndex = 7;
+            this.throttleDown_btn.Text = "↓";
+            this.throttleDown_btn.UseVisualStyleBackColor = true;
+            this.throttleDown_btn.Click += new System.EventHandler(this.throttleDown_btn_Click);
+            // 
+            // throttleUp_btn
+            // 
+            this.throttleUp_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.throttleUp_btn.Location = new System.Drawing.Point(71, 95);
+            this.throttleUp_btn.Name = "throttleUp_btn";
+            this.throttleUp_btn.Size = new System.Drawing.Size(29, 47);
+            this.throttleUp_btn.TabIndex = 8;
+            this.throttleUp_btn.Text = "↑";
+            this.throttleUp_btn.UseVisualStyleBackColor = true;
+            this.throttleUp_btn.Click += new System.EventHandler(this.throttleUp_btn_Click);
+            // 
+            // Throttle_label
+            // 
+            this.Throttle_label.AutoSize = true;
+            this.Throttle_label.Location = new System.Drawing.Point(65, 71);
+            this.Throttle_label.Name = "Throttle_label";
+            this.Throttle_label.Size = new System.Drawing.Size(43, 13);
+            this.Throttle_label.TabIndex = 9;
+            this.Throttle_label.Text = "Throttle";
+            // 
+            // elevator_label
+            // 
+            this.elevator_label.AutoSize = true;
+            this.elevator_label.Location = new System.Drawing.Point(201, 71);
+            this.elevator_label.Name = "elevator_label";
+            this.elevator_label.Size = new System.Drawing.Size(46, 13);
+            this.elevator_label.TabIndex = 10;
+            this.elevator_label.Text = "Elevator";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.airSpeed,
+            this.verticalSpeed,
+            this.pitchAngle});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 282);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.Size = new System.Drawing.Size(283, 58);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Warning,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.dataGridView2.Location = new System.Drawing.Point(12, 209);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView2.Size = new System.Drawing.Size(283, 57);
+            this.dataGridView2.TabIndex = 13;
+            // 
+            // airSpeed
+            // 
+            this.airSpeed.HeaderText = "Air Speed";
+            this.airSpeed.Name = "airSpeed";
+            this.airSpeed.Width = 80;
+            // 
+            // verticalSpeed
+            // 
+            this.verticalSpeed.HeaderText = "Vertical Speed";
+            this.verticalSpeed.Name = "verticalSpeed";
+            this.verticalSpeed.Width = 80;
+            // 
+            // pitchAngle
+            // 
+            this.pitchAngle.HeaderText = "Pitch Angle";
+            this.pitchAngle.Name = "pitchAngle";
+            this.pitchAngle.Width = 80;
+            // 
+            // Warning
+            // 
+            this.Warning.HeaderText = "Warning";
+            this.Warning.Name = "Warning";
+            this.Warning.Width = 80;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Altitude";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 60;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "Throttle";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 50;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "Elevator Pitch";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 50;
+            // 
+            // RemoteFlightController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 393);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.elevator_label);
+            this.Controls.Add(this.Throttle_label);
+            this.Controls.Add(this.throttleUp_btn);
+            this.Controls.Add(this.throttleDown_btn);
+            this.Controls.Add(this.elevatorDown_btn);
+            this.Controls.Add(this.elevatorUp_btn);
+            this.Controls.Add(this.port_label);
+            this.Controls.Add(this.Port_txtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ip_txtBox);
+            this.Controls.Add(this.connect_btn);
+            this.Name = "RemoteFlightController";
+            this.Text = "Remote Plane Controller";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.TextBox ip_txtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Port_txtBox;
+        private System.Windows.Forms.Label port_label;
+        private System.Windows.Forms.Button elevatorUp_btn;
+        private System.Windows.Forms.Button elevatorDown_btn;
+        private System.Windows.Forms.Button throttleDown_btn;
+        private System.Windows.Forms.Button throttleUp_btn;
+        private System.Windows.Forms.Label Throttle_label;
+        private System.Windows.Forms.Label elevator_label;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn airSpeed;
+        private System.Windows.Forms.DataGridViewTextBoxColumn verticalSpeed;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pitchAngle;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Warning;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+    }
+}
+
---
+++ Remote Flight Controller/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Remote_Flight_Controller
+{
+    public delegate void dataHandler(); //delegate
+
+    public partial class RemoteFlightController : Form
+    {
+        struct ControlsUpdate
+        {
+            public ControlsUpdate(int t, int e)
+            {
+                Throttle = t;
+                ElevatorPitch = e;
+            }
+            public double Throttle;         
+            public double ElevatorPitch;    
+        }
+
+        struct TelemetryUpdate
+        {
+            public TelemetryUpdate(int a, int s, int p, int v, int t, int e, int w)
+            {
+                Altitude = a;      
+                Speed = s;         
+                Pitch = p;         
+                VerticalSpeed = v; 
+
+                Throttle = t;      
+                ElevatorPitch = e; 
+
+                WarningCode = w;
+            }
+            public double Altitude;      //Altitude in ft.
+            public double Speed;         //Plane's speed in Knts.
+            public double Pitch;         //Plane's pitch in degrees relative to horizon. Positive is planes pointing upwards, negative plane points downwards;
+            public double VerticalSpeed; //Plane's vertical speed in Feet per minute.
+
+            public double Throttle;      //Current throttle setting as a percentage (i.e. 0% no throttle, 100% full throttle).
+            public double ElevatorPitch; //Current Elevator Pitch in degrees. Positive creates upwards lift, negative downwards.
+
+            public int WarningCode;      //Warning code: 0 - No Warnings; 1 -  Too Low (less than 1000ft); 2 - Stall.
+        }
+
+        ControlsUpdate controlUpdates = new ControlsUpdate(50, 1);
+        TelemetryUpdate telementryUpdates = new TelemetryUpdate(3000, 200, 0, 0, 50, 1, 0);
+
+        public event dataHandler dataReceivedEvent;         
+        public event dataHandler dataSentEvent;              
+        public event dataHandler isThereWarningEvent;        
+
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //initialization of tcp socket 
+        BackgroundWorker backgroundWorker = new BackgroundWorker();                                  //new thread for data transmission
+
+        public RemoteFlightController()
+        {
+            InitializeComponent();
+            backgroundWorker.DoWork += new DoWorkEventHandler(getData);     //backgroundworker is used for data transmission
+            backgroundWorker.DoWork += new DoWorkEventHandler(sendData);
+            dataReceivedEvent += new dataHandler(dataHasReceived);          //when datareceived event occurs,dataHasReceived method will invoke
+            dataSentEvent += new dataHandler(dataHasSent);                       
+            isThereWarningEvent += new dataHandler(invokeWarning);               
+        }
+
+        private void invokeWarning()
+        {
+            if (telementryUpdates.WarningCode == 0)
+            {
+                dataGridView2.Rows[0].Cells[0].Style.ForeColor = Color.Green;
+                dataGridView2.Rows[0].Cells[0].Value = "No Warning";
+            }
+            else if (telementryUpdates.WarningCode == 1)
+            {
+                dataGridView2.Rows[0].Cells[0].Style.ForeColor = Color.Red;
+                dataGridView2.Rows[0].Cells[0].Value = "Too Low";   
+            }
+            else if (telementryUpdates.WarningCode == 2)
+            {
+                dataGridView2.Rows[0].Cells[0].Style.ForeColor = Color.Red;
+                dataGridView2.Rows[0].Cells[0].Value = "Stall";
+            }         
+        }
+
+        private void connectBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress ipAddress = IPAddress.Parse(ip_txtBox.Text);               
+                IPEndPoint remoteApplication = new IPEndPoint(ipAddress, Convert.ToInt32(Port_txtBox.Text));
+                socket.Connect(remoteApplication);                                   
+                backgroundWorker.RunWorkerAsync();          //connection is done, data transmission with seperate tread has started
+                connect_btn.Enabled = false;
+                ip_txtBox.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Couldnt Connect!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }  
+        }
+
+        private void dataHasSent()
+        {
+            
+        }
+
+        private void dataHasReceived()
+        {
+            dataGridView1.Rows[0].Cells[0].Value = telementryUpdates.Speed;
+            dataGridView1.Rows[0].Cells[1].Value = telementryUpdates.VerticalSpeed;
+            dataGridView1.Rows[0].Cells[2].Value = telementryUpdates.Pitch;
+            dataGridView2.Rows[0].Cells[1].Value = telementryUpdates.Altitude;
+            dataGridView2.Rows[0].Cells[2].Value = controlUpdates.Throttle;
+            dataGridView2.Rows[0].Cells[3].Value = controlUpdates.ElevatorPitch;
+        }
+
+        public void getData(object sender, DoWorkEventArgs e)
+        {
+            string incomingData = "";                                                          //incoming data from remote application
+            byte[] bytesBuffer = new Byte[256];                                                //byte array for incoming data 
+            int bytesReceiveCount = socket.Receive(bytesBuffer);                               //amount of bytes stored to int 
+            incomingData = Encoding.ASCII.GetString(bytesBuffer, 0, bytesReceiveCount);        //convertion from byte to string
+            telementryUpdates = JsonConvert.DeserializeObject<TelemetryUpdate>(incomingData);  //json data is tranformed into struct
+            dataReceivedEvent();                                                               //relevant events are invoked
+            isThereWarningEvent();
+            sendData(sender, e);                                                               //repeat listening to socket
+        }
+
+        public void sendData(object sender, DoWorkEventArgs e)
+        {
+            string sendingData = JsonConvert.SerializeObject(controlUpdates);   //data to be sent is converted to json string from struct
+            byte[] bytesBuffer = Encoding.ASCII.GetBytes(sendingData);          //string is converted to byte    
+            socket.Send(bytesBuffer);                                           //Sends data to remote app
+            dataSentEvent();                                                    //relevant event is called
+            getData(sender, e);                                                 //repeat sending
+        }
+
+        private void throttleUp_btn_Click(object sender, EventArgs e)
+        {
+            if(!(controlUpdates.Throttle >= 100))
+            {
+                controlUpdates.Throttle++;
+            }     
+        }
+
+        private void throttleDown_btn_Click(object sender, EventArgs e)
+        {
+            if (!(controlUpdates.Throttle <= 0))
+            {
+                controlUpdates.Throttle--;
+            }
+        }
+
+        private void elevatorUp_btn_Click(object sender, EventArgs e)
+        {
+            if (!(controlUpdates.ElevatorPitch >= 5))
+            {
+                controlUpdates.ElevatorPitch += 0.1;
+            }
+        }
+
+        private void elevatorDown_btn_Click(object sender, EventArgs e)
+        {
+            if (!(controlUpdates.ElevatorPitch <= 0))
+            {
+                controlUpdates.ElevatorPitch -= 0.1;
+            }
+        }
+    }
+}
